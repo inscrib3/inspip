8e0c0a9f1bdd0357c1ed7fb306f62315cddfd1a6
OxMarco <>
2023-10-21 11:23:36 +0200

    catch errors

--- src/screens/modals/show-mnemonic.tsx
+++ src/screens/modals/show-mnemonic.tsx
@@ -1,6 +1,20 @@
-import { Layer, Box, Text, Button } from 'grommet';
+import { Layer, Box, Text, Button, TextInput } from 'grommet';
+import { useState } from 'react';
+import { loadWallet } from '../../lib/wallet';
 
-export function ShowMnemonicModal({ mnemonic, onClose}: { mnemonic: string, onClose: any }) {
+export function ShowMnemonicModal({ onClose}: { onClose: any }) {
+  const [mnemonic, setMnemonic] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const load = () => {
+    try {
+      const wallet = loadWallet(password);
+      setMnemonic(wallet.mnemonic);
+    } catch (e) {
+      alert("Wrong password")
+    }
+  }
+
   return (
         <Layer
           onEsc={onClose}
@@ -16,9 +30,19 @@ export function ShowMnemonicModal({ mnemonic, onClose}: { mnemonic: string, onCl
             round="small"
             background="white"
           >
+            {mnemonic !== "" ? (
             <Text textAlign="center" style={{wordBreak: 'break-all'}}>
               {mnemonic}
             </Text>
+            ) : (
+              <>
+              <TextInput
+                placeholder="Enter password"
+                type="password"
+                onChange={(event) => setPassword(event.target.value)} />
+              <Button label="Show" onClick={() => load()} />
+              </>
+            )}
             <Button label="Close" onClick={onClose} />
           </Box>
         </Layer>
