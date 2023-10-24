675510af0a78f0f07552001945c67bcb86a59dd8
OxMarco <>
2023-10-24 11:37:32 +0200

    save address

--- src/screens/modals/show-address.tsx
+++ src/screens/modals/show-address.tsx
@@ -1,9 +1,20 @@
 import { Layer, Box, Button, Text } from 'grommet';
 import QRCode from 'qrcode.react';
 import { useApp } from '../../app';
+import { useState } from 'react';
 
 export function ShowAddressModal({ onClose }: { onClose: any }) {
   const app = useApp();
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(app.currentAddress);
+      setCopySuccess('Address Copied!');
+    } catch (error) {
+      console.error('Failed to copy: ', error);
+    }
+  };
 
   return (
     <Layer
@@ -24,11 +35,17 @@ export function ShowAddressModal({ onClose }: { onClose: any }) {
       >
         <QRCode value={app.currentAddress} size={128} />
         <Text 
-          style={{ fontFamily: "monospace", maxWidth: "100%", wordWrap: "break-word" }}
+          style={{ fontFamily: "monospace", maxWidth: "100%", wordWrap: "break-word", cursor: "pointer" }}
           margin={{ top: 'small' }}
+          onClick={copyToClipboard}
         >
           {app.currentAddress}
         </Text>
+        {copySuccess && (
+          <Text color="status-ok" size="small">
+            {copySuccess}
+          </Text>
+        )}
         <Button 
           label="Close" 
           onClick={onClose} 
