5d4307f811a044d1c4257e832424de11f15276ba
OxMarco <>
2023-10-25 13:49:21 +0200

    remove mempool

--- src/bitcoin/fees.ts
+++ src/bitcoin/fees.ts
@@ -1,39 +1,3 @@
-import { getFees } from './providers/mempool';
-
-export async function calculateTxBytesFee(
-  vinsLength: number,
-  voutsLength: number,
-  feeRateTier: string,
-  includeChangeOutput: 0 | 1 = 1,
-) {
-  const recommendedFeeRate = await getFees(feeRateTier);
-  return calculateTxBytesFeeWithRate(
-    vinsLength,
-    voutsLength,
-    recommendedFeeRate,
-    includeChangeOutput,
-  );
-}
-
-export function calculateTxBytesFeeWithRate(
-  vinsLength: number,
-  voutsLength: number,
-  feeRate: number,
-  includeChangeOutput: 0 | 1 = 1,
-): number {
-  const baseTxSize = 10;
-  const inSize = 180;
-  const outSize = 34;
-
-  const txSize =
-    baseTxSize +
-    vinsLength * inSize +
-    voutsLength * outSize +
-    includeChangeOutput * outSize;
-  const fee = txSize * feeRate;
-  return fee;
-}
-
 export function estimateFee(vin: bigint, vout: bigint, rate: bigint) {
   return (102n + vin * 112n + vout * 33n) * rate;
 }
