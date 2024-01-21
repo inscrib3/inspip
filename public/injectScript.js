9deb6607cc50b14eb9216b54570a7deac9a1ac5b
redrick0x13 <redrick0x13@protonmail.com>
2024-01-21 18:41:31 +0100

    return connect walletinfo from connect promise working

--- public/injectScript.js
+++ public/injectScript.js
@@ -6,6 +6,17 @@
     const event = new CustomEvent("ConnectWallet");
     window.dispatchEvent(event);
     //in somehow must await until the return of ReturnConnectWalletInfo and then return the values
+    return new Promise((resolve) => {
+      const eventListener = (event) => {
+        if (event.type === "ReturnConnectWalletInfo") {
+          window.removeEventListener("ReturnConnectWalletInfo", eventListener);
+          console.log('success!!!',event.detail)
+          resolve(event.detail); // Resolve the promise with the returned value
+        }
+      };
+
+      window.addEventListener("ReturnConnectWalletInfo", eventListener);
+    });
   };
 
   window.inspip.sendBitcoin = function (toAddress, satoshi, feerate) {
