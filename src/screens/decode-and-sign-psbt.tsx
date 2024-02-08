8b6c994606122721d6747efd720ec73b5688ce03
redrick0x13 <redrick0x13@protonmail.com>
2024-02-08 20:19:22 +0100

    basic signpsbt flow working

--- src/screens/decode-and-sign-psbt.tsx
+++ src/screens/decode-and-sign-psbt.tsx
@@ -4,64 +4,28 @@ import { Box, Button, Footer, Text } from "grommet";
 import { useApp } from "../app";
 import { useEffect, useState } from "react";
 import * as bitcoin from 'bitcoinjs-lib';
-import * as ecc from '@bitcoin-js/tiny-secp256k1-asmjs';
-import { toXOnly } from "../bitcoin/helpers";
-import { ECPairFactory, ECPairAPI} from 'ecpair';
-
-const ECPair: ECPairAPI = ECPairFactory(ecc);
-
-function tweakSigner(signer: bitcoin.Signer, opts: any = {}): bitcoin.Signer {
-  // @ts-ignore
-  let privateKey: Uint8Array | undefined = signer.privateKey!;
-  if (!privateKey) {
-    throw new Error('Private key is required for tweaking signer!');
-  }
-  if (signer.publicKey[0] === 3) {
-    privateKey = ecc.privateNegate(privateKey);
-  }
-
-  const tweakedPrivateKey = ecc.privateAdd(
-    privateKey,
-    tapTweakHash(toXOnly(signer.publicKey), opts.tweakHash),
-  );
-  if (!tweakedPrivateKey) {
-    throw new Error('Invalid tweaked private key!');
-  }
-
-  return ECPair.fromPrivateKey(Buffer.from(tweakedPrivateKey), {
-    network: opts.network,
-  });
-}
-
-function tapTweakHash(pubKey: Buffer, h: Buffer | undefined): Buffer {
-  return bitcoin.crypto.taggedHash(
-    'TapTweak',
-    Buffer.concat(h ? [pubKey, h] : [pubKey]),
-  );
-}
 
 export const DecodeAndSignPsbt = (): JSX.Element => {
   const app = useApp();
   const navigate = useNavigate();
-  const [psbtToSign, setPsbtToSign] = useState<any>(undefined);
+  const [psbtToSign, setPsbtToSign] = useState<bitcoin.Psbt>();
 
   const onSign = async () => {
+    if(!psbtToSign) return;
     try {
-      const tweakedSigner = tweakSigner(app.account.rootKey, { network: bitcoin.networks.testnet });
-      console.log('tweakedSigner',tweakedSigner)
       const inputs = psbtToSign.data.inputs;
-      for await (const input of inputs) {
-        console.log('input',input)
-        await psbtToSign.signInputAsync(input, tweakedSigner);
+      const toSignIndexes = app.signPsbt.toSignInputs.map(el=>el.index);
+      const tweakedChildNode = app.account.account.tweak(
+        bitcoin.crypto.taggedHash('TapTweak', app.account.internalPubkey),
+      );
+      for (let i = 0;i < inputs.length;i++) {
+        if (!toSignIndexes.includes(i)) continue
+        psbtToSign.signInput(i, tweakedChildNode);
         console.log('### sign OK. Psbt:', psbtToSign.toBase64())
+        psbtToSign.finalizeInput(i);
       }
-      psbtToSign.finalizeAllInputs();
-      console.log('after finalizeAllinputs');
-      const tx = psbtToSign.extractTransaction();
-      console.log('myTX: ',tx);
-      const hex = tx.toHex();
-      console.log('myTxHex: ', hex);
-      return hex;
+      const hex = psbtToSign.toHex();
+      return hex;//TODO returnSignPsbt
     } catch (e) {
         console.log((e as Error));
     }
